2d67a974590a3877026e1e32546d1ae6ff8b9c83
priyankasable-625 <priyanka.sable_24uds@sanjivani.edu.in>
2024-12-30 11:11:40 +0530

    hello

---
+++ prg1.java
@@ -0,0 +1,8 @@
+public class prg1{
+    public static void main(String args[]){
+        int sum1 = 10;
+        int sum2 = 20;
+        int sum = numb1 + numb2;
+        System.out.println("sum of number" + sum1 + "and" + sum2 + "is" +sum);
+    }
+}
